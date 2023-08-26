8a766860c3d7da76fce9f7be34c54d93bf71c13f
Kerk Zhan <kerkzhan@gmail.com>
2023-08-26 18:26:10 +0800

    remove localFeed

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import cron from "node-cron";
 import { fetchPosts } from "./fetchPosts";
 import { sendDiscordEmbed } from "./jainbot";
 
+console.log("Starting app...");
 cron.schedule("0 0 * * *", async () => {
   console.log("Fetching RSS feed from fitgirl");
   const newPosts = await fetchPosts();
